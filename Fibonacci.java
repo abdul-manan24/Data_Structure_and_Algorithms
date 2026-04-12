0e7b8a09366436d47dcdb70cb4ad331a4694fc35
abdul-manan24 <manan.cs.2k24@gmail.com>
2026-04-12 21:37:59 +0500

    Initial commit

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+// This is a for loop approach to print 13 first fibonacci terms. 
+
+public class Fibonacci{
+
+    public static void main(String[] args) {
+        int first = 0;
+        int second = 1;
+
+        for (int i=0; i<=12; i++) {
+            int nextFiboNum = first + second;
+
+            System.out.println(first);
+
+            first = second;
+            second = nextFiboNum;
+        }
+    }
+}
