2afd9f8cd8d64fab575174337eda841fb6034bd9
abdul-manan24 <manan.cs.2k24@gmail.com>
2026-04-16 14:41:42 +0500

    Pushing selectionSort code

---
+++ selectionSort.java
@@ -0,0 +1,24 @@
+public class selectionSort {
+    public static void main(String[] args) {
+
+        int[] myNumbers = {7, 9, 11, 12, 3};
+
+        for (int i=0; i<myNumbers.length; i++) {
+            int minIndex = i;
+            for (int j=i + 1; j<myNumbers.length; j++) {
+                if (myNumbers[j] < myNumbers[minIndex]) {
+                    minIndex = j;
+                }
+
+                int temp = myNumbers[i];
+                myNumbers[i] = myNumbers[minIndex];
+                myNumbers[minIndex] = temp;
+            }
+        }
+
+        System.out.print("Sorted list" + " ");
+        for (int k=0; k<myNumbers.length; k++) {
+            System.out.print(myNumbers[k] + " ");
+        }
+    }
+}
